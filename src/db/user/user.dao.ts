851dcd07be562a4c3446bd7030966a148dfed7f5
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2023-11-01 19:19:54 +0100

    remove validation from UserDao

--- src/db/user/user.dao.ts
+++ src/db/user/user.dao.ts
@@ -1,19 +1,6 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
 export class UserDao {
-  @IsNotEmpty()
-  @IsString()
   _id: string;
-
-  @IsNotEmpty()
-  @IsString()
   email: string;
-
-  @IsNotEmpty()
-  @IsNumber()
   exp: number;
-
-  @IsNotEmpty()
-  @IsNumber()
   iat: number;
 }
